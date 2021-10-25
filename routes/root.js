8d581001d368a36317992d25c7a55ac9d4828c05
Jamie <jamie@lambertstock.com>
2021-10-25 20:50:05 +0000

    Added in google sheets

--- routes/root.js
+++ routes/root.js
@@ -1,26 +1,46 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const { convert } = require('html-to-text');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 const fs = require('fs');
 require('dotenv').config();
+const creds = require('../config/google_config.json');
 
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE,
+  secure: false,
   auth: {
     user: process.env.STMP_USER,
     pass: process.env.STMP_PASS,
   },
+  tls: {
+    ciphers: 'SSLv3',
+  },
 });
 
 router.get('/', async (req, res) => res.render('index.ejs'));
-router.get('/thanks', (req, res) => res.render('thanks.ejs'));
+router.get('/thanks', async (req, res) => res.render('thanks.ejs'));
 
 router.post('/', async (req, res) => {
-  console.log(req.body);
+  const doc = new GoogleSpreadsheet('1_FMcyxf3U4MSLukeROz_eVwPLhcml88YolEWJzFr6Ps');
+  await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+
+  // Add data to sheet
+  await sheet.addRow({
+    Name: req?.body?.christmasName,
+    Email: req?.body?.christmasEmail,
+    Phone: req?.body?.christmasPhone,
+    Starter: req?.body?.christmasStarter,
+    Main: req?.body?.christmasMain,
+    Dessert: req?.body?.christmasDessert,
+  });
+
+  await sheet.saveUpdatedCells();
 
   // Get HTML Email
   let html = fs.readFileSync('public/emails/new-submission.html', 'utf8');
